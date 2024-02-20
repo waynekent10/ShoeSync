cfd007cf4d04caf40be86ffdca66f32fa16d462d
Bruce Clark <Bruce.clark55@gmail.com>
2024-02-19 22:03:30 -0500

    added creator form

---
+++ components/forms/CreatorForm.js
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
+import { useAuth } from '../../utils/context/authContext';
+import { createCreator, updateCreator } from '../../api/creatorData';
+
+const initialState = {
+  name: '',
+  company: '',
+  bio: '',
+  uid: '',
+};
+
+function CreatorForm({ obj }) {
+  const [formInput, setFormInput] = useState(initialState);
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (obj.firebaseKey) setFormInput(obj);
+  }, [obj, user]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firebaseKey) {
+      updateCreator(formInput).then(() => router.push(`/author/${obj.firebaseKey}`));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createCreator(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+        updateCreator(patchPayload).then(() => {
+          router.push('/creators');
+        });
+      });
+    }
+  };
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Creator</h2>
+
+      <FloatingLabel controlId="floatingInput1" label="Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter Creator Name"
+          name="name"
+          value={formInput.name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* IMAGE INPUT  */}
+      <FloatingLabel controlId="floatingInput2" label="Creator Image" className="mb-3">
+        <Form.Control
+          type="url"
+          placeholder="Enter an image url"
+          name="image"
+          value={formInput.image}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingInput1" label="Company" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter Company"
+          name="company"
+          value={formInput.name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingTextarea" label="bio" className="mb-3">
+        <Form.Control
+          as="textarea"
+          placeholder="Bio"
+          style={{ height: '100px' }}
+          name="description"
+          value={formInput.bio}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* SUBMIT BUTTON  */}
+      <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Creator</Button>
+    </Form>
+  );
+}
+
+CreatorForm.propTypes = {
+  obj: PropTypes.shape({
+    name: PropTypes.string,
+    image: PropTypes.string,
+    bio: PropTypes.string,
+    company: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }),
+};
+
+CreatorForm.defaultProps = {
+  obj: initialState,
+};
+
+export default CreatorForm;
