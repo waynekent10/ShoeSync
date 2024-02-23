9f6de796642f24b074a07f01742018c3db7a4801
Bruce Clark <Bruce.clark55@gmail.com>
2024-02-22 21:35:16 -0500

    made corrections for authprovider

---
+++ pages/creators.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../utils/context/authContext';
+import CreatorCard from '../components/CreatorCard';
+import { getCreators } from '../api/creatorData';
+
+export default function Creator() {
+  const [creators, setCreators] = useState([]);
+  const { user } = useAuth();
+
+  const getAllCreators = () => {
+    getCreators(user.uid).then(setCreators);
+  };
+  useEffect(() => {
+    getAllCreators();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  return (
+    <div>
+      {creators.map((creator) => (
+        <CreatorCard key={creator.firebaseKey} creatorObj={creator} onUpdate={getAllCreators} />
+      ))}
+    </div>
+  );
+}
