4fd1865e39db5f17faacb4627db1cd653835ca52
Bruce Clark <Bruce.clark55@gmail.com>
2024-02-24 19:38:20 -0500

    added collection

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { Button } from 'react-bootstrap';
 import { getSneakers } from '../api/shoeData';
 import SneakerCard from '../components/SneakerCard';
 
@@ -18,10 +16,7 @@ function Home() {
 
   return (
     <div className="text-center my-4">
-      <Link href="/sneaker/new" passHref>
-        <Button>Add Shoe</Button>
-      </Link>
-      <div>
+      <div className="d-flex flex-wrap">
         {sneakers.map((sneaker) => (
           <SneakerCard key={sneaker.firebaseKey} sneakerObj={sneaker} onUpdate={getAllTheSneakers} />
         ))}
