42ddb5d74a67b2de4ae2d4f2c03ce3e0dad37bff
Bruce Clark <Bruce.clark55@gmail.com>
2024-03-13 15:20:18 -0400

    corrected data

---
+++ components/forms/BrandForm.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
+import { useAuth } from '../../utils/context/authContext';
+import { createBrand, updateBrand } from '../../api/brandData';
+
+const initialState = {
+  name: '',
+  bio: '',
+  image: '',
+  favorite: '',
+};
+
+function BrandForm({ obj }) {
+  const [formInput, setFormInput] = useState(initialState);
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (obj.firebaseKey) setFormInput(obj);
+  }, [obj, user]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firebaseKey) {
+      updateBrand(formInput).then(() => router.push(`/brand/${obj.firebaseKey}`));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createBrand(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+        updateBrand(patchPayload).then(() => {
+          router.push('/brand');
+        });
+      });
+    }
+  };
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Brand</h2>
+
+      <FloatingLabel controlId="floatingInput1" label="Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter Brand Name"
+          name="name"
+          value={formInput.name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* IMAGE INPUT  */}
+      <FloatingLabel controlId="floatingInput2" label="Brand Image" className="mb-3">
+        <Form.Control
+          type="url"
+          placeholder="Enter an image url"
+          name="image"
+          value={formInput.image}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingTextarea" label="bio" className="mb-3">
+        <Form.Control
+          as="textarea"
+          placeholder="Bio"
+          style={{ height: '100px' }}
+          name="description"
+          value={formInput.bio}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      <Form.Check
+        className="text-white mb-3"
+        type="switch"
+        id="favorite"
+        name="favorite"
+        label="Favorite?"
+        checked={formInput.favorite}
+        onChange={(e) => {
+          setFormInput((prevState) => ({
+            ...prevState,
+            favorite: e.target.checked,
+          }));
+        }}
+      />
+
+      {/* SUBMIT BUTTON  */}
+      <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Brand</Button>
+    </Form>
+  );
+}
+
+BrandForm.propTypes = {
+  obj: PropTypes.shape({
+    name: PropTypes.string,
+    image: PropTypes.string,
+    bio: PropTypes.string,
+    favorite: PropTypes.bool,
+    firebaseKey: PropTypes.string,
+  }),
+};
+
+BrandForm.defaultProps = {
+  obj: initialState,
+};
+
+export default BrandForm;
