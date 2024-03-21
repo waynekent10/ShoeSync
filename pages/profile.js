bbc6de6d164cf1c3727fca39720e20b4ebe1749d
Bruce Clark <Bruce.clark55@gmail.com>
2024-03-21 13:40:49 -0400

    added userProfile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,16 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
-import { useAuth } from '../utils/context/authContext';
+import UserProfile from '../components/UserProfile';
 
-export default function UserProfile() {
-  const { user } = useAuth();
-
-  return (
-    <div>
-      <Image src={user.photoURL} alt="userURL" width="100px" height="100px" />
-      <h1>{user.displayName}</h1>
-      <h2>{user.email}</h2>
-      <h3>{user.metadata.lastSignInTime}</h3>
-    </div>
-  );
+export default function Profile() {
+  return <div><UserProfile /></div>;
 }
